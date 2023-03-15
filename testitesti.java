59985079ed965ee46bde565cbab2f5536ab814a3
Junnu Danhammer <115397736+JunnuD@users.noreply.github.com>
2023-03-15 12:35:45 +0200

    Create testitesti.java

---
+++ testitesti.java
@@ -0,0 +1,13 @@
+public class testitesti {
+    public static void main(String[] args) {
+        int[] lista = {1, 2, 3, 4, 5};
+
+        for (int i = 0; lista.length < 5; i++ ) {
+            while (i < 5) {
+                System.out.println("moikkuli");
+            }
+        }
+    }
+}
+
+// Ei toimi kuitenkaan
